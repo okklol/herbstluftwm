4712d121048f761b00ed1f066379c6e6e4d420c6
okklol <97268506+okklol@users.noreply.github.com>
2022-03-24 21:33:39 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# colors
+
+GREEN="\x1B[32m"
+YELLOW="\x1B[33m"
+BLUE="\x1B[34m"
+CYAN="\033[36m"
+RED="\033[1;31m"
+WHITE="\x1B[37m"
+BOLD="\x1B[1m"
+RESET="\x1B[0m"
+
+# this bash script will install my herblufstwm dots
+# order of operations:
+
+
+# 1 - greet the user
+# 2 - update and install dependencies
+# 3 - clone https://github.com/okklol/herblufstwm/
+# 4 - move ~/herblufstwm to ~/,config/herblufstwm
+echo -e "hello $USER"
+echo -e "this script will install my herblufstwm dots to your computer"
+echo -e "first, an explanetion on what this script will do:"
+echo -e "1 - upstate and install dependencies \n2 - clone my herb dots github repo (https://github.com/okklol/herblufstwm/) \n3 - move the herblufstwm folder to .config"
+
+echo -e "now, lets start, do you wish to proceed? ${GREEN}Y${RESET}/${RED}N${RESET}"
